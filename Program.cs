74481d0a09f8154b11bd3510c4e3acd746cea373
Taufan Augusta <taufan.augusta@protonmail.com>
2022-02-22 18:42:29 +0700

    add 1000 users

--- Program.cs
+++ Program.cs
@@ -1,4 +1,23 @@
-﻿using Generator;
+﻿using System.Text.Json;
+using Generator;
 
-string password = Randomizer.GeneratePassword(8);
-Console.WriteLine($"password: {password}");
+List<User> Users = new();
+
+for (int i = 0; i < 1000; i++)
+{
+    string password = Randomizer.GeneratePassword(8);
+    Console.WriteLine($"password: {password}");
+
+    Users.Add(
+        new User{
+            Username = $"respondent{i}",
+            password = password
+        }
+    );
+}
+
+var format = new JsonSerializerOptions { WriteIndented = true };
+string json = JsonSerializer.Serialize<List<User>>(Users, format);
+
+string directory = System.IO.Directory.GetCurrentDirectory();
+File.WriteAllText($"{directory}\\generated-user.json", json);
---
+++ User.cs
@@ -0,0 +1,10 @@
+#nullable disable
+
+namespace Generator
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string password { get; set; }
+    }
+}
